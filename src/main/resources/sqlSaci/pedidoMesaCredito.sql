9352981f050e3d1265633801853ad79ce8d05700
ivaneyvieira <ivaneyvieira@gmail.com>
2020-10-15 09:59:07 -0300

    Libera loja 5

--- src/main/resources/sqlSaci/pedidoMesaCredito.sql
+++ src/main/resources/sqlSaci/pedidoMesaCredito.sql
@@ -33,7 +33,7 @@ FROM sqldados.eord             AS O
   INNER JOIN TMETODO_CREDIARIO AS M
 	       USING (paymno)
 WHERE (O.date >= @DATA AND O.status IN (:statusSaci))
-  AND O.storeno IN (3, 8, 9, 12);
+  AND O.storeno IN (3, 5, 8, 9, 12);
 
 DROP TABLE IF EXISTS TSIMULADOR;
 CREATE TEMPORARY TABLE TSIMULADOR (
